bf43b8fddd2f06c4173afd3070ecaaa441e558b9
ysun01 <wendy.sun@oath.com>
2018-12-12 17:36:47 +0800

    Prune and add comments

--- src/server.js
+++ src/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const Store = require('./Store');
-const mockedToDos = require('./mockedToDos');
 const ToDos = require('./ToDos');
-const connect = require('./connect');
-const createPageHTML = require('./createPageHTML');
+const { connect, createPageHTML, mockedToDos } = require('./utils');
 
 const server = express();
 
 server.get('/', (req, res) => {
+    // create a store
     const store = new Store();
+
+    // populate the store with initial data
     store.populateState({ todos: mockedToDos });
 
+    // connect app to store
+    // app props dealt in connect
     const App = connect(ToDos, store);
+
+    // dehydrate & send to client
     const storeState = store.getState();
     const pageHTML = createPageHTML(App, storeState);
-
     res.send(pageHTML);
 });
 
---
+++ src/utils.js
@@ -0,0 +1,99 @@
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+// connect
+
+class Container extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = { updateTime: 0 };
+        this.props.store.registerCallback(this.update);
+    }
+
+    update () {
+        const { updateTime } = this.state;
+        updateTime++;
+        this.setState({ updateTime });
+    }
+
+    render () {
+        const { children, getViewProps } = this.props;
+        return React.createElement(children, getViewProps(), null);
+    }
+};
+
+const connect = (View, store) => {
+    const getViewProps = () => ({
+        todos: store.getState().todos,
+        toggleToDo: store.toggleToDo
+    });
+
+    const props = { getViewProps, store };
+    return React.createElement(Container, props, View);
+};
+
+
+// createHTML
+
+const createPageHTML = (App, storeState) => `
+    <html>
+        <head>
+            <title>ToDos</title>
+            <style>
+                body {
+                    font-family: monospace;
+                    padding: 100px;
+                    text-align: center;
+                }
+
+                h1 {
+                    font-size: 40px;
+                }
+
+                .todo {
+                    cursor: pointer;
+                    font-size: 30px;
+                    line-height: 40px;
+                }
+
+                .todo.completed {
+                    opacity: 0.2;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>ToDos</h1>
+            <div class="root">${ReactDOMServer.renderToString(App)}</div>
+            <script>
+                window.storeState = ${JSON.stringify(storeState)};
+            </script>
+        </body>
+    </html>
+`;
+
+// mockedToDos
+
+const mockedToDos = [
+    {
+        id: 1,
+        title: 'wash car',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'read novel',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'cook dinner',
+        completed: true
+    },
+    {
+        id: 4,
+        title: 'walk dog',
+        completed: false
+    }
+];
+
+module.exports = { connect, createPageHTML, mockedToDos };
