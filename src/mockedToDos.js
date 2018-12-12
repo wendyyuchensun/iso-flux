02efbc8dcca37cd5d979d73a1369b1577cd2ebc7
ysun01 <wendy.sun@oath.com>
2018-12-12 16:35:38 +0800

    Add mockedToDos

---
+++ src/mockedToDos.js
@@ -0,0 +1,24 @@
+const mockedToDos = [
+    {
+        id: 1,
+        title: 'wash car',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'read novel',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'cook dinner',
+        completed: false
+    },
+    {
+        id: 4,
+        title: 'walk dog',
+        completed: false
+    }
+];
+
+module.exports = mockedToDos;
